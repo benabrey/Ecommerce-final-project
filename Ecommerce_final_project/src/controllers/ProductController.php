dd20d988f36b6358ec24ffa06f5fdababceab73c
benabrey <80432663+benabrey@users.noreply.github.com>
2025-12-03 12:53:36 -0800

    Added header and footer to show method

--- Ecommerce_final_project/src/controllers/ProductController.php
+++ Ecommerce_final_project/src/controllers/ProductController.php
@@ -59,7 +59,10 @@ class ProductController {
 
         $relatedProducts = $this->productModel->getByCategory($product['category'], 4);
 
+        require_once __DIR__ . '/../views/header.php';
+        require_once __DIR__ . '/../views/navbar.php';
         require_once __DIR__ . '/../views/product_detail.php';
+        require_once __DIR__ . '/../views/footer.php';
     }
 
     /**
