2278c0c0941686657e22d2109082510715e38bef
benabrey <80432663+benabrey@users.noreply.github.com>
2025-12-03 15:09:22 -0800

    Checkout files, new "terms and conditions" file

--- Ecommerce_final_project/public/checkout.php
+++ Ecommerce_final_project/public/checkout.php
@@ -1,1 +1,22 @@
 <?php
+// Checkout page
+
+ require_once __DIR__ . '/../config/database.php';
+
+ require_once __DIR__ . '/../src/controllers/CartController.php';
+ require_once __DIR__ . '/../src/helpers/Session.php';
+ require_once __DIR__ . '/../src/helpers/Auth.php';
+
+ Session::start();
+
+ Auth::requireAuth();
+
+ $db = getDatabaseConnection();
+
+ $cartController = new CartController($db);
+
+ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+     $cartController->processCheckout();
+ } else {
+     $cartController->showCheckout();
+ }
